16127eb6e638f29b80724cbd1094ba0c59e869d1
aurelia-mp <aurelia.porolli@gmail.com>
2023-05-02 12:30:31 -0300

    cambios minimos para deploy

--- src/daos/carritos/CarritosDaoMongoDb.js
+++ src/daos/carritos/CarritosDaoMongoDb.js
@@ -1,4 +1,4 @@
-import ContenedorMongoDb from "../../contenedores/ContenedorMongoDb.js"
+import ContenedorMongoDb from "../../contenedores/contenedorMongoDb.js"
 
 class CarritosDaoMongoDb extends ContenedorMongoDb {
 
